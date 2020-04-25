a11e97000d5ebe7422de60ce71228db1647d6392
Rinat <37953890+khusainovrm@users.noreply.github.com>
2020-04-25 12:02:43 +0200

    navbar

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="sidenav app-sidenav open">
+
+    <router-link 
+    v-for="link in links"
+    :key="link.url"
+    tag="li" 
+    active-class="active"
+    exact
+    :to="link.url"
+    >
+      <a href="#" class="waves-effect waves-orange pointer">{{ link.title }}</a>
+    </router-link>
+</ul>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    links: [
+      {title:"Счет", url: "/home"},
+      {title:"История", url: "/history"},
+      {title:"Планирование", url: "/planing"},
+      {title:"Новая запись", url: "/record"},
+      {title:"Категории", url: "/categories"}  
+    ]
+  })
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,63 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+      path: '/login',
+      name: 'login',
+      meta: {layout: "empty"},
+      component: () => import ('../views/Login.vue')
+    },
+    {
+      path: '/detail/:id',
+      name: 'detail',
+      meta: {layout: "main"},
+      component: () => import ("../views/DetailRrecord.vue")
+    },
+    {
+      path: '/categories',
+      name: 'categories',
+      meta: {layout: "main"},
+      component: () => import ("../views/Categories.vue")
+    },
+    {
+      path: '/histoty',
+      name: 'histoty',
+      meta: {layout: "main"},
+      component: () => import ("../views/History.vue")
+    },
+    {
+      path: '/home',
+      name: 'home',
+      meta: {layout: "main"},
+      component: () => import ("../views/Home.vue")
+    },
+    {
+      path: '/planing',
+      name: 'planing',
+      meta: {layout: "main"},
+      component: () => import ("../views/Planing.vue")
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      meta: {layout: "main"},
+      component: () => import ("../views/Profile.vue")
+    },
+    {
+      path: '/record',
+      name: 'record',
+      meta: {layout: "main"},
+      component: () => import ("../views/Record.vue")
+    },
+    {
+      path: '/register',
+      name: 'register',
+      meta: {layout: "empty"},
+      component: () => import ("../views/Register.vue")
+    }
 ]
 
 const router = new VueRouter({
