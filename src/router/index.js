aef6a2f6d43ed1fa32a44381005af27131017abe
Rinat <37953890+khusainovrm@users.noreply.github.com>
2020-05-03 14:09:44 +0300

    защита роутов

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import firebase from "firebase/app"
 
 Vue.use(VueRouter)
 
-  const routes = [
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
     {
       path: '/login',
       name: 'login',
@@ -13,43 +18,43 @@ Vue.use(VueRouter)
     {
       path: '/detail/:id',
       name: 'detail',
-      meta: {layout: "main"},
+      meta: {layout: "main", auth: true},
       component: () => import ("../views/DetailRrecord.vue")
     },
     {
       path: '/categories',
       name: 'categories',
-      meta: {layout: "main"},
+      meta: {layout: "main", auth: true},
       component: () => import ("../views/Categories.vue")
     },
     {
       path: '/history',
       name: 'histoty',
-      meta: {layout: "main"},
+      meta: {layout: "main", auth: true},
       component: () => import ("../views/History.vue")
     },
     {
       path: '/',
       name: 'home',
-      meta: {layout: "main"},
+      meta: {layout: "main", auth: true},
       component: () => import ("../views/Home.vue")
     },
     {
       path: '/planing',
       name: 'planing',
-      meta: {layout: "main"},
+      meta: {layout: "main", auth: true},
       component: () => import ("../views/Planing.vue")
     },
     {
       path: '/profile',
       name: 'profile',
-      meta: {layout: "main"},
+      meta: {layout: "main", auth: true},
       component: () => import ("../views/Profile.vue")
     },
     {
       path: '/record',
       name: 'record',
-      meta: {layout: "main"},
+      meta: {layout: "main", auth: true},
       component: () => import ("../views/Record.vue")
     },
     {
@@ -58,12 +63,18 @@ Vue.use(VueRouter)
       meta: {layout: "empty"},
       component: () => import ("../views/Register.vue")
     }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+  ]
 })
+
+router.beforeEach(((to, from, next) => {
+  const currentUser = firebase.auth().currentUser
+  const requireAuth = to.matched.some(record => record.meta.auth)
+
+  if (requireAuth && !currentUser) {
+    next('/login?message=login')
+  } else {
+    next()
+  }
+}))
 
 export default router
--- src/utils/message.js
+++ src/utils/message.js
@@ -1,5 +1,6 @@
 export default {
   "logout" : "Вы вышли из системы",
+  "login" : "Войдите в систему",
   "auth/wrong-password" : "Неправильный пароль",
   "auth/user-not-found" : "Пользователь не зарегистрирован",
   "auth/too-many-requests" : "Слишком много неудачных попыток, попробуйте позднее",
